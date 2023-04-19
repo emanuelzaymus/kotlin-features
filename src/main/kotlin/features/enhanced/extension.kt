500db040b127de6c38507c8eb8e0072ab0e3de90
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2023-04-19 22:43:39 +0200

    extensions

--- src/main/kotlin/features/enhanced/extension.kt
+++ src/main/kotlin/features/enhanced/extension.kt
@@ -1,110 +1,119 @@
 package features.enhanced
 
 import java.util.WeakHashMap
+import kotlin.random.Random
 
-interface User {
+interface Person {
     val id: Long
     val firstName: String
     val lastName: String
 }
 
-data class ProUser(
+data class User(
     override val id: Long,
     override val firstName: String,
     override val lastName: String,
-) : User
+) : Person
 
-val User.initials: String
-    get() = "${firstName[0]}.${lastName[0]}"
+val Person.initials: String
+    get() = "${firstName[0]}.${lastName[0]}."
 
-private val user: User = ProUser(1, "Janko", "Fric")
+private val user: Person = User(1, "Janko", "Fric")
 
 fun userWithClassicExtension() {
-    println("User: $user")
+    println(user)
     // User: ProUser(id=1, firstName=Janko, lastName=Fric)
 
     println("Initials: ${user.initials}")
-    // Initials: J.F
+    // Initials: J.F.
 }
 
-data class UserWithEmail(
-    private val user: User,
-    val email: String,
-) : User by user
+data class UserWithPhoneNumber(
+    private val person: Person,
+    val phoneNumber: String,
+) : Person by person
 
-fun userWithEmail() {
-    val userWithEmail = UserWithEmail(user, "janko.fric@prosoft.sk")
+fun userWithPhoneNumber() {
+    val userWithPhoneNumber = UserWithPhoneNumber(user, "+421 123 456 789")
 
-    println(userWithEmail.id) // 1
-    println(userWithEmail.firstName) // Janko
-    println(userWithEmail.lastName) // Fric
-    println(userWithEmail.email) // janko.fric@prosoft.sk
+    println(userWithPhoneNumber.id) // 1
+    println(userWithPhoneNumber.firstName) // Janko
+    println(userWithPhoneNumber.lastName) // Fric
+    println(userWithPhoneNumber.phoneNumber) // +421 123 456 789
 }
 
-private val weakReferenceEmails = WeakHashMap<User, String>()
+// region Enhanced Extension Property
 
-var User.email: String?
-    get() = weakReferenceEmails[this]
+private val weakReferenceAges = WeakHashMap<Person, Int>()
+
+var User.age: Int?
+    get() = weakReferenceAges[this]
     set(value) {
-        weakReferenceEmails[this] = value
+        weakReferenceAges[this] = value
     }
 
-fun userWithEnhancedExtension() {
-    val proUser = ProUser(2, "Jozko", "Mrkvicka")
-    println("Users email: " + proUser.email)
-    // Users email: null
+// endregion
 
-    proUser.email = "jozko.mrkvicka@prosoft.sk"
-    println("Users email after update: " + proUser.email)
-    // Users email after update: jozko.mrkvicka@prosoft.sk
+fun userWithEnhancedExtension() {
+    val user = User(2, "Jozko", "Mrkvicka")
+    println(user.age)
+    // null
+
+    user.age = 30
+    println(user.age)
+    // 30
 }
 
 fun moreUsersWithEnhancedExtension() {
-    println("Emails count: " + weakReferenceEmails.size)
-    // Emails count: 0
+    println("Ages count: " + weakReferenceAges.size)
+    // Ages count: 0
 
     val users = List(100_000) { index ->
-        ProUser(index.toLong(), "Jozko", "Mrkvicka")
+        User(index.toLong(), "XX", "YY")
     }
     users.forEach {
-        it.email = "${it.firstName}.${it.lastName}@prosoft.sk"
+        it.age = Random.nextInt(1, 100)
     }
 
-    println("Emails count: " + weakReferenceEmails.size)
-    // Emails count: 100000
+    println("Ages count: " + weakReferenceAges.size)
+    // Ages count: 100000
 
-    val lastUser = ProUser(3, "Janko", "Hrasko")
-    lastUser.email = "janko.hrasko@prosoft.sk"
+    val lastUser = User(3, "Jozko", "Mrkvicka")
+    lastUser.age = 29
 
-    println("Emails count: " + weakReferenceEmails.size)
-    // Emails count: 100001
+    println("Ages count: " + weakReferenceAges.size)
+    // Ages count: 100001
 
 
     repeat(5) {
         System.gc()
     }
 
-    println("Emails count: " + weakReferenceEmails.size)
+    println("Ages count: " + weakReferenceAges.size)
     // Emails count: 93781
 
     repeat(5) {
         System.gc()
     }
 
-    println("Emails count: " + weakReferenceEmails.size)
-    // Emails count: 1
+    println("Ages count: " + weakReferenceAges.size)
+    // Ages count: 1
 
-    println("Last email: " + lastUser.email)
-    // Last email: janko.hrasko@prosoft.sk
+    println("Last age: " + lastUser.age)
+    // Last age: 29
 }
 
 
 fun main() {
     userWithClassicExtension()
     println()
-    userWithEmail()
+
+    userWithPhoneNumber()
     println()
+
     userWithEnhancedExtension()
     println()
+
+    weakReferenceAges.clear()
     moreUsersWithEnhancedExtension()
 }
